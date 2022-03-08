fa3af692b9d55d754f1b4cb6961a3d3d6c788dbe
edsonjfs <edsonjosefs@gmail.com>
2022-03-08 13:45:15 -0300

    feat: Implementado o uso de Colletctions

--- src/banco/Conta.java
+++ src/banco/Conta.java
@@ -1,28 +1,37 @@
 package banco;
 
-public class Conta {
+public class Conta implements Comparable<Conta> {
+   protected String tipo;
    protected String numeroConta;
    protected String agencia;
    protected double saldo = 0;
    protected Cliente cliente;
-
     public Conta() {
     }
-
+    @Override
+    public int compareTo(Conta conta) {
+        double diferenca = this.saldo - conta.saldo ;
+        if (diferenca == 0) return 0 ;
+        if (diferenca > 0) return -1;
+        else return 1;
+    }
     public Conta(Cliente cliente) {
         this.cliente = cliente;
     }
-
     public void depositar(double valor) {
         saldo += valor;
     }
-
     public void sacar(double valor) {
         saldo -= valor;
     }
-
     public void transferir(double valor, Conta contaDestino) {
         this.sacar(valor);
         contaDestino.depositar(valor);
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public Cliente getCliente() {
+        return cliente;
     }
 }
--- src/banco/ContaCorrente.java
+++ src/banco/ContaCorrente.java
@@ -2,7 +2,12 @@ package banco;
 
 public class ContaCorrente extends Conta {
 
+    String tipo = "Corrente";
     public ContaCorrente (Cliente cliente) {
         super(cliente);
+    }
+    @Override
+    public String toString() {
+        return "Conta " + tipo + " do " + cliente + "Saldo: " + saldo;
     }
 }
